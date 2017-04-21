259f4b5fdafb6aa7028c1f544f16cdf30d6bf044
miguelshevchuk <shevchuk.miguel@gmail.com>
2017-04-20 23:39:36 -0300

    Algunos agregados de la entrega 2

--- entrega1.hs
+++ entrega1.hs
@@ -2,45 +2,51 @@ import Text.Show.Functions
 
 type Nombre = String
 type Vida = Int
-type Ataque = Protagonista -> Protagonista
+type Reaccion = (Protagonista -> Protagonista) -> Protagonista  -> Protagonista
+type SeresQueridos = [Protagonista]
 
-data Protagonista = Protagonista {nombre::Nombre, vida::Vida } deriving(Show)
-data Zombie = Zombie {ataqueZombie::Ataque} deriving(Show)
+data Protagonista = Protagonista {nombre::Nombre, vida::Vida, reaccion::Reaccion, seresQueridos::SeresQueridos } deriving(Show)
 
-setVidaProtagonista vidaNueva protagonista = protagonista{vida = vidaNueva}
+instance Eq Protagonista where
+  protagonista1 == protagonista2 =
+    nombre protagonista1 == nombre protagonista2 &&
+    vida protagonista1 == vida protagonista2
+
+
+nuevaVidaProtagonista vidaNueva protagonista = protagonista{vida = vidaNueva}
+nuevosSeresQueridosProtagonista amigosNuevos protagonista = protagonista{seresQueridos = amigosNuevos}
 
 --Protagonistas EJERCICIO 1
-daryl = Protagonista {nombre = "Daryl", vida = 55}
-maggie = Protagonista {nombre = "Maggie", vida = 100}
-carol = Protagonista {nombre = "Carol", vida = 200}
+daryl = Protagonista {nombre = "Daryl", vida = 55, seresQueridos = [carol], reaccion = sacrificarse}
+maggie = Protagonista {nombre = "Maggie", vida = 100, seresQueridos = [carol, daryl, krilin], reaccion = caerse}
+carol = Protagonista {nombre = "Carol", vida = 200, seresQueridos = [victor], reaccion = matarZombie}
+victor = Protagonista {nombre = "Victor Sueiro", vida = 1, seresQueridos = [], reaccion = sacrificarse}
+krilin = Protagonista {nombre = "Krilin", vida = 1, seresQueridos = [], reaccion = sacrificarse}
 
 --Zombies EJERCICIO 2
-zombieTranqui = Zombie {ataqueZombie = ataqueTranqui}
-zombieConCasco = Zombie {ataqueZombie = ataqueZombieConCasco}
-zombieSinDientes = Zombie {ataqueZombie = ataqueSinDientes}
 
--- Ataques Zombies
-ataqueTranqui protagonista = setVidaProtagonista (vida protagonista -10) protagonista
-ataqueZombieConCasco protagonista= setVidaProtagonista (div (vida protagonista) 2) protagonista
-ataqueSinDientes = id
+zombieTranqui protagonista= nuevaVidaProtagonista (vida protagonista -10) protagonista
+zombieConCasco protagonista= nuevaVidaProtagonista (div (vida protagonista) 2) protagonista
+zombieSinDientes = id
 
--- Funcion -> pasar zombie y protagonista
-ataqueAProtagonista = ataqueZombie
+zombieBuenaso hambre protagonista | (length.seresQueridos) protagonista > hambre = zombieSinDientes protagonista
+                                  | otherwise = zombieConCasco protagonista
 
--- EJERCICIO 3: ataqueAProtagnista zombieTranqui (ataqueAProtagonista zombieConCasco carol)
+zombieReSacado protagonista = nuevosSeresQueridosProtagonista ((map zombieTranqui.map zombieConCasco.seresQueridos) protagonista) (zombieTranqui protagonista)
+
+estaMuerto protagonista = (vida protagonista) == 0 && not(any (==krilin) (seresQueridos protagonista)) && not(any (==victor) (seresQueridos protagonista))
+
+-- EJERCICIO 3: (zombieTranqui.zombieConCasco) carol
+--Resultado: Protagonista (nombre = "Carol", vida = 90)
 
 --ACCIONES EJERCICIO 4
 
-matarZombie protagonista zombie = protagonista
-caerse protagonista zombie = ataqueAProtagonista zombie (ataqueAProtagonista zombie protagonista)
-sacrificarse protagonista zombie =  setVidaProtagonista 0 protagonista
+matarZombie zombie = id
+caerse zombie = zombie.zombie
+sacrificarse zombie =  nuevaVidaProtagonista 0
 
--- Segunda opcion
+-- EJERCICIO 5: sacrificarse zombieTranqui carol
+--Resultado: Protagonista (nombre = "Carol", vida = 0)
 
--- matarZombie = id
--- caerse protagonista zombie = ataqueAProtagonista zombie (ataqueAProtagonista zombie protagonista)
--- sacrificarse =  setVidaProtagonista 0
-
--- EJERCICIO 5: sacrificarse carol zombieTranqui
-
--- EJERCICIO 6: caerse (matarZombie maggie zombieSinDientes) zombieConCasco
+-- EJERCICIO 6: (caerse  zombieConCasco.matarZombie zombieSinDientes) maggie
+--Resultado: Protagonista (nombre = "Maggie", vida = 25)
