fa0f4e1ea5b361c396d24f4bbd880bdca1f90fd6
miguelshevchuk <shevchuk.miguel@gmail.com>
2017-04-22 02:53:32 -0300

    agrego flip y foldl

--- entrega1.hs
+++ entrega1.hs
@@ -66,7 +66,7 @@ pelearseConProtagonistas (x:xs) protagonista = pelearseConProtagonistas xs ((nue
 -- HORDAS
 
 ataqueHorda::AtaqueHorda
-ataqueHorda zombies protagonista = foldr ($) protagonista (map (reaccion protagonista) zombies)
+ataqueHorda zombies protagonista = foldl (flip ($)) protagonista (map (reaccion protagonista) zombies)
 
 --Ejercicio 4 Entrega 2
 estaMuerto protagonista = (vida protagonista) <= 0 && not(any (==krilin) (seresQueridos protagonista)) && not(any (==victor) (seresQueridos protagonista))
@@ -123,14 +123,14 @@ eaccion = <function>, seresQueridos = []}]}]}]}
 
 4) ataqueHorda [zombieTranqui, zombieAstuto 20, zombieReSacado, zombieAstuto 1, zombieTranqui] maggie
 
-Resultado: Protagonista {nombre = "Maggie", vida = -5, reaccion = <function>, seresQueridos
- = [Protagonista {nombre = "Carol", vida = 35, reaccion = <function>, seresQueri
-dos = [Protagonista {nombre = "Victor Sueiro", vida = 1, reaccion = <function>,
-seresQueridos = []}]},Protagonista {nombre = "Daryl", vida = -2, reaccion = <fun
-ction>, seresQueridos = [Protagonista {nombre = "Carol", vida = 200, reaccion =
-<function>, seresQueridos = [Protagonista {nombre = "Victor Sueiro", vida = 1, r
-eaccion = <function>, seresQueridos = []}]}]},Protagonista {nombre = "Krilin", v
-ida = -15, reaccion = <function>, seresQueridos = []}]}
+Resultado: Protagonista {nombre = "Maggie", vida = -20, reaccion = <function>, seresQuerido
+s = [Protagonista {nombre = "Carol", vida = 35, reaccion = <function>, seresQuer
+idos = [Protagonista {nombre = "Victor Sueiro", vida = 1, reaccion = <function>,
+ seresQueridos = []}]},Protagonista {nombre = "Daryl", vida = -2, reaccion = <fu
+nction>, seresQueridos = [Protagonista {nombre = "Carol", vida = 200, reaccion =
+ <function>, seresQueridos = [Protagonista {nombre = "Victor Sueiro", vida = 1,
+reaccion = <function>, seresQueridos = []}]}]},Protagonista {nombre = "Krilin",
+vida = -15, reaccion = <function>, seresQueridos = []}]}
 
 5) (map nombre.filter estaMuerto.seresQueridos.ataqueHorda [zombieTranqui, zombieAstuto 20, zombieReSacado, zombieAstuto 1, zombieTranqui]) maggie
 
